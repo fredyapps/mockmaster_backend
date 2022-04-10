c1a3d7c25c400618d3d5a599c8f28941c45cd45b
frederick sallah <fredysallah@gmail.com>
2022-04-10 15:53:11 +0000

    fixing questions query wait time

--- src/main/java/com/themockmaster/tmmpayment/repository/OptionRepository.java
+++ src/main/java/com/themockmaster/tmmpayment/repository/OptionRepository.java
@@ -12,7 +12,7 @@ import com.themockmaster.tmmpayment.mockmodels.Question;
 
 public interface OptionRepository  extends JpaRepository<Option, Long> {
 	
-	@Query(value="SELECT * FROM option  WHERE question_id = ?1", nativeQuery = true)
+	@Query(value="SELECT * FROM `option`  WHERE question_id = ?1", nativeQuery = true)
 	List<Option> findOptionsByquestion(Integer questionId);
 
 }
